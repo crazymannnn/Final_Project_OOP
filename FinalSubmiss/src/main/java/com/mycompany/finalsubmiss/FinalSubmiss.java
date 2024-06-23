6df1b0ec889695b7b43d41c442557c82d7d6026a
eiuThanhDuy <duynguyen120504@gmail.com>
2024-06-23 11:32:44 +0700

    1
    1

---
+++ FinalSubmiss/src/main/java/com/mycompany/finalsubmiss/FinalSubmiss.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.finalsubmiss;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class FinalSubmiss {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
